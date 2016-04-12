966091d8d89b82df4018efac9993e9786b309d0d
Milosz Galazka <milosz@sleeplessbeastie.eu>
2016-04-12 22:13:14 +0200

    Added function to read UMASK without changing it permanently from http://www.gnu.org/software/libc/manual/html_node/Setting-Permissions.html

--- explain-umask.c
+++ explain-umask.c
@@ -1,7 +1,18 @@
 #include <stdlib.h>
+#include <sys/stat.h>
+
+mode_t read_umask(void);
 
 int main()
 {
   // exit
   exit(0);
+}
+
+mode_t read_umask(void) {
+  // how to read the mask with umask without changing it permanently:
+  // http://www.gnu.org/software/libc/manual/html_node/Setting-Permissions.html
+  mode_t mask = umask (0);
+  umask (mask);
+  return mask;
 }
