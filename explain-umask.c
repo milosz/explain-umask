51d119f5928d730a237f99bba2bdc64270304089
Milosz Galazka <milosz@sleeplessbeastie.eu>
2016-04-12 22:20:24 +0200

    Added octal UMASK representation

--- explain-umask.c
+++ explain-umask.c
@@ -1,10 +1,29 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <sys/stat.h>
 
 mode_t read_umask(void);
 
 int main()
 {
+  // mask value
+  mode_t mask;
+
+  // extracted values
+  int octal_mask_user, octal_mask_group, octal_mask_other;
+
+  // read umask
+  mask=read_umask();
+
+  // calculate octal values
+  octal_mask_user=mask >> 6;          //  XXX000000
+  octal_mask_group=(mask & 56) >> 3;  //  000XXX000
+  octal_mask_other=mask & 7;          //  000000XXX
+
+  // print octal representation
+  printf("Octal    representation: %i%i%i\n", \
+	 octal_mask_user, octal_mask_group, octal_mask_other);
+
   // exit
   exit(0);
 }
